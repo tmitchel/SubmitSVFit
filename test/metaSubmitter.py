ff6b0586cb6e70f1fcf0d4b59002fa34b496007c
Tyler Mitchell <tyler106@ksu.edu>
2018-08-17 09:31:46 -0500

    utility script to run svFitSubmitter.py on all subdirectories of a given directory

---
+++ test/metaSubmitter.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="run on a directory containing directories containing the skimmed ntuples")
+parser.add_argument('-d', '--sampledir', action = 'store', help = 'path to the directory')
+parser.add_argument('-p', '--prefix', action = 'store', help = 'name to prefix all directories')
+args = parser.parse_args()
+
+sampledir = args.sampledir
+prefix = args.prefix
+
+dirlist = ['%s%s' % (sampledir, sample) for sample in os.listdir(sampledir)]
+for idir in dirlist:
+  iswj = 0
+  if 'WJets' in idir:
+    iswj = 1
+#  print('python svFitSubmitter.py -sd %s -es=1 -iswj=%i -mt=-1 --jobName %s/svfit_dir_%s' % (idir, iswj, prefix, idir.split('/')[-1]))
+  subprocess.call('python svFitSubmitter.py -sd %s -es=1 -iswj=%i -mt=-1 --jobName %s/svfit_dir_%s' % (idir, iswj, prefix, idir.split('/')[-1]), shell=True)
