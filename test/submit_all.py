a7f7dba8c67a977a10cfcd82945341e90ab2807c
Tyler Mitchell <tyler106@ksu.edu>
2019-09-24 14:03:18 -0500

    add submit_all script

---
+++ test/submit_all.py
@@ -0,0 +1,19 @@
+import os
+import condor_handler as ch
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="run on a directory containing directories containing the skimmed ntuples")
+parser.add_argument('-d', '--sampledir', action = 'store', help = 'path to the directory')
+parser.add_argument('-p', '--prefix', action = 'store', help = 'name to prefix all directories')
+args = parser.parse_args()
+
+sampledir = args.sampledir
+prefix = args.prefix
+
+dirlist = ['%s/%s' % (sampledir, sample) for sample in os.listdir(sampledir)]
+for idir in dirlist:
+  command =  '$CMSSW_BASE/bin/$SCRAM_ARCH/SVFitStandAloneFSATauDM doES=1 metType=-1 inputfile=$value newFile=\'$OUTPUT\''
+  if 'WJets' in idir:
+    command += ' isWJets=1 '
+  jobName = '{}/svfit_dir_{}'.format(args.prefix, idir.split('/')[-1])
+  ch.submit_command(command, jobName=jobName, input_sample_dir=idir)
