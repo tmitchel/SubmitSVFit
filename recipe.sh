a89f7e29350dcd0c0210489b3fdde4b5c07a3b43
Tyler Mitchell <tyler106@ksu.edu>
2019-12-20 09:17:58 -0600

    update recipe to checkout newer svfit with fastmtt

--- recipe.sh
+++ recipe.sh
@@ -1,6 +1,6 @@
 pushd $CMSSW_BASE/src
 
-git clone https://github.com/SVfit/ClassicSVfit TauAnalysis/ClassicSVfit
+git clone -b fastMTT_19_02_2019https://github.com/SVfit/ClassicSVfit TauAnalysis/ClassicSVfit
 git clone https://github.com/SVfit/SVfitTF TauAnalysis/SVfitTF
 git clone https://github.com/CMS-HTT/RecoilCorrections.git  HTT-utilities/RecoilCorrections 
 
